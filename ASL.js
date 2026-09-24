b88ab8772374b9c55fe868913c12bd32e92126f8
ahmm123 <ambrnamhnwys@gmail.com>
2026-09-24 15:45:25 +0330

    fixed some bogs and wrote the ComponentManager

--- ASL.js
+++ ASL.js
@@ -1,14 +1,12 @@
 class Component {
-  constructor(name, componentId, filePath, javascript, css) {
+  constructor(name, filePath, javascript, css) {
     this.name = name;
     this.filePath = filePath;
     this.css = css;
     this.javascript = javascript;
   }
-  state = {"h": "h"};
-  getName = () => {
-    return this.name;
-  }
+  rendered = false;
+  state = {};
   getState = () => {
     return this.state;
   }
@@ -21,15 +19,18 @@ class Component {
   changeStateByKey = (key, value) => {
     this.state[key] = value;
   }
-  contains = (key) => {
-    if (this.state[key]) {
+  containsInState = (key) => {
+    if (Object.hasOwn(this.state, key)) {
       return true;
     }
     return false;
   }
   render = async (originQuery) => {
+    if (this.rendered) {
+      return;
+    }
     await fetch(this.filePath)
-    .then(res => res.text())
+    .then(res => { if (res.ok) { return res.text(); } else { throw new Error(`error: ${this.filePath} didn\`t found.`) } })
     .then(data => {
       const origin = document.querySelector(String(originQuery));
       if (origin) {
@@ -38,21 +39,23 @@ class Component {
         this.javascript.forEach(info => {
           const script = document.createElement('script');
           script.src = info.src;
-          script.classList = `${this.name}-script`;
-          if (info.isModue) script.type = "module";
+          script.classList.add(`${this.name}-script`);
+          if (info.isModule) script.type = "module";
           let scriptOrigin = info.inHead ? document.head : document.body;
           scriptOrigin.appendChild(script);
         });
         this.css.forEach(src => {
-          const style = document.createElement('style');
-          style.src = src;
-          style.classList = `${this.name}-style`
-          document.head.appendChild(script);
+          const style = document.createElement('link');
+          style.rel = 'stylesheet';
+          style.href = src;
+          style.classList.add(`${this.name}-style`);
+          document.head.appendChild(style);
         });
-        doc.querySelectorAll('*').forEach(e=>e.classList.add(`${this.name}`));
+        doc.querySelectorAll('*').forEach(e => e.classList.add(`${this.name}`));
         origin.append(...doc.body.childNodes);
+        this.rendered = true;
       } else {
-        throw `error: ${originQuery} didn\`t found in this page`;
+        throw new Error(`error: ${originQuery} didn\`t found in this page`);
       }
     })
     .catch(err => {
@@ -60,6 +63,52 @@ class Component {
     });
   }
   remove = () => {
-    document.querySelectorAll(`${this.name}, ${this.name}-script, ${this.name}-style`).forEach(e => e.remove());
+    document.querySelectorAll(`.${this.name}, .${this.name}-script, .${this.name}-style`).forEach(e => e.remove());
+  };
+}
+
+class ComponentManager {
+  components = {};
+  addComponent = (name, filePath, javascript, css) => {
+    this.components[name] = new Component(name, filePath, javascript, css);
+  };
+  render = (name, originQuery) => {
+    if (this.components[name]) {
+      this.components[name].render(originQuery);
+    }
+  };
+  getState = (name) => {
+    if (this.components[name]) {
+      return this.components[name].getState();
+    }
+  }
+  addToState = (name, key, value) => {
+    if (this.components[name]) {
+      this.components[name].addToState(key, value);
+    }
+  }
+  getStateByKey = (name, key) => {
+    if (this.components[name]) {
+      return this.components[name].getStateByKey(key);
+    }
+  }
+  changeStateByKey = (name, key, value) => {
+    if (this.components[name]) {
+      this.components[name].changeStateByKey(key, value);
+    }
+  }
+  containsInState = (name, key) => {
+    if (this.components[name]) {
+      return this.components[name].containsInState(key);
+    }
+  }
+  removeComponent = (name) => {
+    if (this.components[name]) {
+      this.components[name].remove();
+      delete this.components[name];
+    }
+  }
+  addComponentByObject = (componentObject) => {
+    this.components[componentObject.name] = componentObject;
   };
 }
