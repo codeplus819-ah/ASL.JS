2f98655360a5e80b1fd7e16bf3ffd1f0330806b3
ahmm123 <ambrnamhnwys@gmail.com>
2026-09-24 15:53:05 +0330

    made Component private to ComponentManager

--- ASL.js
+++ ASL.js
@@ -1,76 +1,75 @@
-class Component {
-  constructor(name, filePath, javascript, css) {
-    this.name = name;
-    this.filePath = filePath;
-    this.css = css;
-    this.javascript = javascript;
-  }
-  rendered = false;
-  state = {};
-  getState = () => {
-    return this.state;
-  }
-  addToState = (key, value) => {
-    this.state[key] = value;
-  }
-  getStateByKey = (key) => {
-    return this.state[key];
-  }
-  changeStateByKey = (key, value) => {
-    this.state[key] = value;
-  }
-  containsInState = (key) => {
-    if (Object.hasOwn(this.state, key)) {
-      return true;
-    }
-    return false;
-  }
-  render = async (originQuery) => {
-    if (this.rendered) {
-      return;
-    }
-    await fetch(this.filePath)
-    .then(res => { if (res.ok) { return res.text(); } else { throw new Error(`error: ${this.filePath} didn\`t found.`) } })
-    .then(data => {
-      const origin = document.querySelector(String(originQuery));
-      if (origin) {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(data, 'text/html');
-        this.javascript.forEach(info => {
-          const script = document.createElement('script');
-          script.src = info.src;
-          script.classList.add(`${this.name}-script`);
-          if (info.isModule) script.type = "module";
-          let scriptOrigin = info.inHead ? document.head : document.body;
-          scriptOrigin.appendChild(script);
-        });
-        this.css.forEach(src => {
-          const style = document.createElement('link');
-          style.rel = 'stylesheet';
-          style.href = src;
-          style.classList.add(`${this.name}-style`);
-          document.head.appendChild(style);
-        });
-        doc.querySelectorAll('*').forEach(e => e.classList.add(`${this.name}`));
-        origin.append(...doc.body.childNodes);
-        this.rendered = true;
-      } else {
-        throw new Error(`error: ${originQuery} didn\`t found in this page`);
-      }
-    })
-    .catch(err => {
-      console.error(err);
-    });
-  }
-  remove = () => {
-    document.querySelectorAll(`.${this.name}, .${this.name}-script, .${this.name}-style`).forEach(e => e.remove());
-  };
-}
-
 class ComponentManager {
+  #Component = class Component {
+    constructor(name, filePath, javascript, css) {
+      this.name = name;
+      this.filePath = filePath;
+      this.css = css;
+      this.javascript = javascript;
+    }
+    rendered = false;
+    state = {};
+    getState = () => {
+      return this.state;
+    }
+    addToState = (key, value) => {
+      this.state[key] = value;
+    }
+    getStateByKey = (key) => {
+      return this.state[key];
+    }
+    changeStateByKey = (key, value) => {
+      this.state[key] = value;
+    }
+    containsInState = (key) => {
+      if (Object.hasOwn(this.state, key)) {
+        return true;
+      }
+      return false;
+    }
+    render = async (originQuery) => {
+      if (this.rendered) {
+        return;
+      }
+      await fetch(this.filePath)
+      .then(res => { if (res.ok) { return res.text(); } else { throw new Error(`error: ${this.filePath} didn\`t found.`) } })
+      .then(data => {
+        const origin = document.querySelector(String(originQuery));
+        if (origin) {
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(data, 'text/html');
+          this.javascript.forEach(info => {
+            const script = document.createElement('script');
+            script.src = info.src;
+            script.classList.add(`${this.name}-script`);
+            if (info.isModule) script.type = "module";
+            let scriptOrigin = info.inHead ? document.head : document.body;
+            scriptOrigin.appendChild(script);
+          });
+          this.css.forEach(src => {
+            const style = document.createElement('link');
+            style.rel = 'stylesheet';
+            style.href = src;
+            style.classList.add(`${this.name}-style`);
+            document.head.appendChild(style);
+          });
+          doc.querySelectorAll('*').forEach(e => e.classList.add(`${this.name}`));
+          origin.append(...doc.body.childNodes);
+          this.rendered = true;
+        } else {
+          throw new Error(`error: ${originQuery} didn\`t found in this page`);
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    }
+    remove = () => {
+      document.querySelectorAll(`.${this.name}, .${this.name}-script, .${this.name}-style`).forEach(e => e.remove());
+    };
+  }
   components = {};
   addComponent = (name, filePath, javascript, css) => {
-    this.components[name] = new Component(name, filePath, javascript, css);
+    this.components[name] = new this.#Component(name, filePath, javascript, css);
   };
   render = (name, originQuery) => {
     if (this.components[name]) {
@@ -108,7 +107,4 @@ class ComponentManager {
       delete this.components[name];
     }
   }
-  addComponentByObject = (componentObject) => {
-    this.components[componentObject.name] = componentObject;
-  };
 }
